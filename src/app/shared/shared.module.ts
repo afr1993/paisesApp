bd712107f37c1a990535edc2bb8288fb092981bb
afr1993 <31361041+afr1993@users.noreply.github.com>
2023-09-01 02:10:23 -0700

    link route implementado

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent,
+    AboutPageComponent,
+    SideBarComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports:[
+    HomePageComponent,
+    AboutPageComponent,
+    SideBarComponent
   ]
 })
 export class SharedModule { }
